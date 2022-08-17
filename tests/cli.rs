ad531d08c14768fbde68783a14e22c5da12ad925
Pawel Lisewski <pawel.lisewski@tuta.io>
2022-08-17 11:58:43 +0800

    pass test: run

--- tests/cli.rs
+++ tests/cli.rs
@@ -9,3 +9,9 @@ fn dies_no_args() {
         .failure()
         .stderr(predicate::str::contains("USAGE"));
 }
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("hello").assert().success();
+}
